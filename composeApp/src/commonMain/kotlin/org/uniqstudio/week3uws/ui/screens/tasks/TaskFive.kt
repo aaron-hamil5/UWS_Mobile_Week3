305ae9c07e128400a15641ebd04fd19855f3b832
Aaron Hamilton (MacBook Air) <aaronleon28@gmail.com>
2026-02-12 16:09:04 +0000

    Task Five Complete

--- composeApp/src/commonMain/kotlin/org/uniqstudio/week3uws/ui/screens/tasks/TaskFive.kt
+++ composeApp/src/commonMain/kotlin/org/uniqstudio/week3uws/ui/screens/tasks/TaskFive.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import org.uniqstudio.week3uws.ui.screens.premade.TextForUI
 import org.uniqstudio.week3uws.ui.screens.premade.TopBar
 import org.uniqstudio.week3uws.ui.screens.premade.UserInputShellCompact
 import uws_mobile_week3.composeapp.generated.resources.Res
@@ -16,7 +17,7 @@ fun TaskFive(
     onClickBack: () -> Unit,
     onClickNext: () -> Unit,
 ){
-
+    val fruit = listOf("Apple", "Banana", "Orange")
 
     Scaffold(
         modifier = modifier,
@@ -33,7 +34,11 @@ fun TaskFive(
                 description = "Understand how to use the for-each loop to iterate over a collection.",
                 infoText = "Task Five",
                 composableContent = {
-
+                    fruit.forEach {
+                        TextForUI(
+                            text = it
+                        )
+                    }
                 },
                 onClickBack = onClickBack,
                 onClickNext = onClickNext
