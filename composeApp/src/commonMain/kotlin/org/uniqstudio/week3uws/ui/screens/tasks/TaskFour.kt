952a189dc9ee804f04fe81fc8ec16bf214e5d86c
Aaron Hamilton (MacBook Air) <aaronleon28@gmail.com>
2026-02-12 16:06:08 +0000

    Task Four Complete

--- composeApp/src/commonMain/kotlin/org/uniqstudio/week3uws/ui/screens/tasks/TaskFour.kt
+++ composeApp/src/commonMain/kotlin/org/uniqstudio/week3uws/ui/screens/tasks/TaskFour.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import org.uniqstudio.week3uws.ui.screens.premade.TextForUI
 import org.uniqstudio.week3uws.ui.screens.premade.TopBar
 import org.uniqstudio.week3uws.ui.screens.premade.UserInputShellCompact
 import uws_mobile_week3.composeapp.generated.resources.Res
@@ -31,7 +32,11 @@ fun TaskFour(
                 description = "Learn to use the for loop to iterate over a range of numbers.",
                 infoText = "Task Four",
                 composableContent = {
-
+                    for (i in 1..5){
+                        TextForUI(
+                            text = "Iteration $i"
+                        )
+                }
                 },
                 onClickBack = onClickBack,
                 onClickNext = onClickNext
