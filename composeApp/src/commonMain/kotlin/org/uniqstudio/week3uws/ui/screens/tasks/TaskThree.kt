a986576b1f47b3c26040b55566a3f16414e43fb1
Aaron Hamilton (MacBook Air) <aaronleon28@gmail.com>
2026-02-12 16:04:37 +0000

    Task Three Complete

--- composeApp/src/commonMain/kotlin/org/uniqstudio/week3uws/ui/screens/tasks/TaskThree.kt
+++ composeApp/src/commonMain/kotlin/org/uniqstudio/week3uws/ui/screens/tasks/TaskThree.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import org.uniqstudio.week3uws.ui.screens.premade.TextForUI
 import org.uniqstudio.week3uws.ui.screens.premade.TopBar
 import org.uniqstudio.week3uws.ui.screens.premade.UserInputShellCompact
 import uws_mobile_week3.composeapp.generated.resources.Res
@@ -16,6 +17,7 @@ fun TaskThree(
     onClickBack: () -> Unit,
     onClickNext: () -> Unit,
 ){
+    val mark = 85
     Scaffold(
         modifier = modifier,
         topBar = { TopBar(
@@ -31,7 +33,18 @@ fun TaskThree(
                 description = "Practice using the if-else-if ladder and the when statement for more complex decision-making.",
                 infoText = "Task Three",
                 composableContent = {
-
+                    TextForUI(text = "The grade is:")
+                    TextForUI(text = if (mark >= 90) "A" else if (mark >= 80) "B" else if (mark >= 70) "C" else if (mark >= 60) "D" else "F")
+                    TextForUI(text =
+                        when {
+                            mark >= 90 -> "A"
+                            mark >= 80 -> "B"
+                            mark >= 70 -> "C"
+                            mark >= 60 -> "D"
+                            else -> "F"
+                        }
+                    )
+                    TextForUI(text = "The mark is $mark")
                 },
                 onClickBack = onClickBack,
                 onClickNext = onClickNext
