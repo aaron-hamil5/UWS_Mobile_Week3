b9da2b4933fcfe0cc90f050e2aab8e2135e09810
Aaron <aaronleon28@gmail.com>
2026-02-13 18:38:11 +0000

    Exercise Two Completed

--- composeApp/src/commonMain/kotlin/org/uniqstudio/week3uws/ui/screens/exercise/ExerciseTwo.kt
+++ composeApp/src/commonMain/kotlin/org/uniqstudio/week3uws/ui/screens/exercise/ExerciseTwo.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import org.uniqstudio.week3uws.ui.screens.premade.TextForUI
 import org.uniqstudio.week3uws.ui.screens.premade.TopBar
 import org.uniqstudio.week3uws.ui.screens.premade.UserInputShellCompact
 import uws_mobile_week3.composeapp.generated.resources.Res
@@ -16,6 +17,7 @@ fun ExerciseTwo(
     onClickBack: () -> Unit,
     onClickNext: () -> Unit,
 ){
+    val num = 10
     Scaffold(
         modifier = modifier,
         topBar = { TopBar(
@@ -31,6 +33,12 @@ fun ExerciseTwo(
                 description = "Understand how to use the if-else statement for decision-making.",
                 infoText = "Exercise Two",
                 composableContent = {
+                    TextForUI(
+                        text = if (num % 2 == 0) "Number is even - $num" else "Number is odd - $num"
+                    )
+                    TextForUI(
+                        text = if (num % 5 == 0) "Number is dividable by 5 - $num" else "Number is not dividable by 5 - $num"
+                    )
                 },
                 onClickBack = onClickBack,
                 onClickNext = onClickNext
